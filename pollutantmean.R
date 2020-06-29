33119cbf1e90c59e1eae04da3559bb6831647335
remolloy1010 <remolloy101@gmail.com>
2020-06-28 18:04:49 -0600

    added functionality to add leading zeros to csv file name, added directory input

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,4 +1,4 @@
-pollutantmean <- function(pollutant, id=1:332){
+pollutantmean <- function(directory, pollutant, id=1:332){
     ## 'directory' is a character vector of length 1 indicating the location of the CSV files
     
     ## 'pollutant' is a character vector of length 1 indicating the name of the pollutant for which we will calculate the mean; either "sulfate" or "nitrate"
@@ -9,6 +9,9 @@ pollutantmean <- function(pollutant, id=1:332){
     
     ## NOTE: do not round result!
     
+    ## Change working directory to specified path
+    newDirectory <- paste('./', directory, sep="")
+    setwd(newDirectory)
 
     # Read csv file & output data table
     for (num in id) {
@@ -29,6 +32,9 @@ pollutantmean <- function(pollutant, id=1:332){
     # Remove NA and find mean of pollutant data
     pollutantArrayClean <- complete.cases(dataTable[pollutant])
     meanPollutant <- mean(pollutantArrayClean)
+    
+    # Change working directory back to original path
+    setwd("C:/Users/10294643/Documents/R/Data Science - Foundations using R Specialization/Course 2 - R Programming")
     
     return (meanPollutant)
 }
