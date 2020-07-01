691a40826330ca76d0e72135668659f36c7b669b
remolloy1010 <remolloy101@gmail.com>
2020-06-30 19:17:07 -0600

    changed function to read id as vector, create pollutants vector, accurately takes individual means

--- pollutantmean.R
+++ pollutantmean.R
@@ -3,38 +3,44 @@ pollutantmean <- function(directory, pollutant, id=1:332){
     
     ## 'pollutant' is a character vector of length 1 indicating the name of the pollutant for which we will calculate the mean; either "sulfate" or "nitrate"
     
-    ## 'id' is an integer vector indicating teh monitor ID numbers to be used
+    ## 'id' is an integer vector indicating the monitor ID numbers to be used
     
     ## Return the mean of the pollutant across all monitors list in the 'id' vector (ignoring NA values)
     
     ## NOTE: do not round result!
     
-    ## Change working directory to specified path
-    newDirectory <- paste('./', directory, sep="")
-    setwd(newDirectory)
-
-    # Read csv file & output data table
+    # Initialize array of means from pollutant data
+    meanPollutantArray <- c()
+    
+    # Loop through each id specified
     for (num in id) {
-        if (id < 10) {
-            fileID <- paste('00',id,sep='')
-        }
-        else if (id < 100) {
-            fileID <- paste('0',id,sep='')
-        }
-        else {
-            fileID <- id
-        }
+        print(num)
+    
+        # Read csv file & output data table
+        fileID <- formatC(num, width=3, flag="0")
+        filename <- paste('./', directory, '/', fileID, '.csv', sep='')
+        dataTable <- read.csv(filename)
+        print(dataTable)
+        
+        # Remove NA and find mean of pollutant data
+        dataTableClean <- complete.cases(dataTable)
+        #print(pollutantArrayClean)
+        #meanPollutant2 <- mean(dataTable[pollutantArrayClean,]))
+        
+        pollutantArrayClean <- dataTable[dataTableClean,][pollutant]
+        print(pollutantArrayClean)
+        
+        #meanPollutant <- mean(pollutantArrayClean)
+        meanPollutant <- mean(as.matrix(pollutantArrayClean))
+        print(meanPollutant)
+        meanPollutantArray <- append(meanPollutantArray, meanPollutant)
+        print(meanPollutantArray)
+        meanAllMonitors <- mean(meanPollutantArray)
+        print(meanAllMonitors)
+        
     }
     
-    filename <- paste(fileID, 'csv', sep='.')
-    dataTable <- read.csv(filename)
-    
-    # Remove NA and find mean of pollutant data
-    pollutantArrayClean <- complete.cases(dataTable[pollutant])
-    meanPollutant <- mean(pollutantArrayClean)
-    
-    # Change working directory back to original path
-    setwd("C:/Users/10294643/Documents/R/Data Science - Foundations using R Specialization/Course 2 - R Programming")
-    
     return (meanPollutant)
+    
+    
 }
